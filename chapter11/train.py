863101eef4fbcb8f2eb2762d084bb285355fa440
eogus <kddhh2021@gmail.com>
2025-07-31 11:31:27 +0900

    add compute_mse_and_acc() and implementing train()

--- chapter11/train.py
+++ chapter11/train.py
@@ -4,9 +4,6 @@ from sklearn.model_selection import train_test_split
 from NeuralNetMLP import NeuralNetMLP
 from NeuralNetMLP import int_to_onehot
 
-num_epochs = 50
-minibatch_size = 100
-
 def minibatch_generator(X, y, minibatch_size):
      indices = np.arange(X.shape[0])
      np.random.shuffle(indices)
@@ -24,28 +21,70 @@ def mse_loss(targets, probas, num_labels=10):
 def accuracy(targets, predicted_labels):
      return np.mean(predicted_labels == targets)
 
-model = NeuralNetMLP(num_features=28*28,
+def compute_mse_and_acc(nnet, X, y, num_labels=10, minibatch_size=100):
+     mse, correct_pred, num_examples = 0.0, 0, 0
+     minibatch_gen = minibatch_generator(X, y, minibatch_size)
+     
+     for i, (features, targets) in enumerate(minibatch_gen):
+          _, probas = nnet.forward(features)
+          predicted_labels = np.argmax(probas, axis=1)
+          onehot_targets = int_to_onehot(
+               targets, num_labels=num_labels
+          )
+          loss = np.mean((onehot_targets - probas) ** 2)
+          correct_pred += (predicted_labels == targets).sum()
+          num_examples += targets.shape[0]
+          mse += loss
+     
+     mse = mse/i
+     acc = correct_pred / num_examples
+     
+     return mse, acc
+
+def train(model, X_train, y_train, X_valid, y_valid, num_epochs,
+          learning_rate=0.1, minibatch_size=100):
+     epoch_loss = []
+     epoch_train_acc = []
+     epoch_valid_acc = []
+     
+     for e in range(num_epochs):
+          minibatch_gen = minibatch_generator(
+               X_train, y_train, minibatch_size
+          )
+          for X_train_mini, y_train_mini in minibatch_gen:
+               a_h, a_out = model.forward(X_train_mini)
+
+               d_loss__d_w_out, d_loss__d_b_out, \
+               d_loss__d_w_h, d_loss__d_b_h = \
+                    model.backward(X_train_mini, a_h, a_out,
+                                   y_train_mini)
+               
+               model.weight_h -= learning_rate * d_loss__d_w_h
+               model.bias_h -= learning_rate * d_loss__d_b_h
+               model.weight_out -= learning_rate * d_loss__d_w_out
+               model.bias_out -= learning_rate * d_loss__d_b_out
+          
+          # train_mse, train_acc = 
+
+
+if __name__ == '__main__':
+     model = NeuralNetMLP(num_features=28*28,
                      num_hidden=50,
                      num_classes=10)
 
-X = np.load('data/mnist_data_X.npy')
-y = np.load('data/mnist_target_y.npy')
+     X = np.load('data/mnist_data_X.npy')
+     y = np.load('data/mnist_target_y.npy')
 
-X = ((X / 255.) - 0.5) * 2
+     X = ((X / 255.) - 0.5) * 2
 
-X_temp, X_test, y_temp, y_test = train_test_split(
-    X, y, test_size=10000, random_state=123, stratify=y
-)
-X_train, X_valid, y_train, y_valid = train_test_split(
-    X_temp, y_temp, test_size=5000,
-    random_state=123, stratify=y_temp
-)
+     X_temp, X_test, y_temp, y_test = train_test_split(
+     X, y, test_size=10000, random_state=123, stratify=y
+     )
+     X_train, X_valid, y_train, y_valid = train_test_split(
+     X_temp, y_temp, test_size=5000,
+     random_state=123, stratify=y_temp
+     )
 
-_, probas = model.forward(X_valid)
-mse = mse_loss(y_valid, probas)
-print(f'validation set MSE: {mse:.1f}')
-
-predicted_labels = np.argmax(probas, axis=1)
-### ↑ 행을 기준으로 최대값 추출 shape = (5000, )
-acc = accuracy(y_valid, predicted_labels)
-print(f'validation set Accuracy: {acc*100:.1f}%')
+     mse, acc = compute_mse_and_acc(model, X_valid, y_valid)
+     print(f'Validation Set MSE: {mse:.1f}')
+     print(f'Validation Set Accuracy: {acc*100:.1f}%')
